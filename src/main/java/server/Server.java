21601def348c0abf281f4d514cdfcf4112501a0f
Dixie Dean <d.w.sokolov26@gmail.com>
2023-07-26 14:04:15 +0300

    ThreadPool added

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -3,19 +3,24 @@ package server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.List;
+import java.util.concurrent.*;
 
 public class Server {
     private static final List<String> validPaths = List.of("/index.html",
             "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js",
             "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+    private final Executor threadPool;
+
+    public Server() {
+        threadPool = Executors.newFixedThreadPool(64);
+    }
 
     public void launch() {
         try (ServerSocket serverSocket = new ServerSocket(9999)) {
             System.out.println("Server is running...");
             while (!serverSocket.isClosed()) {
                 RequestHandler handler = new RequestHandler(serverSocket.accept());
-                Thread thread = new Thread(handler);
-                thread.start();
+                threadPool.execute(handler);
             }
         } catch (IOException exception) {
             exception.printStackTrace();
