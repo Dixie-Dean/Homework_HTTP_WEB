4896d5a6f3a3939aa5f9167928f1f945a0559a26
Dixie Dean <d.w.sokolov26@gmail.com>
2023-07-31 16:20:40 +0300

    RequestBuilder added

--- src/main/java/request/Request.java
+++ src/main/java/request/Request.java
@@ -1,15 +1,20 @@
 package request;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class Request {
     private final String method;
     private final String path;
+    private final List<String> headers;
+    private final String body;
     private final HashMap<String, String> key;
 
-    public Request(String method, String path) {
+    public Request(String method, String path, List<String> headers, String body) {
         this.method = method;
         this.path = path;
+        this.headers = headers;
+        this.body = body;
         this.key = new HashMap<>();
         this.key.put(method, path);
     }
---
+++ src/main/java/request/RequestBuilder.java
@@ -0,0 +1,128 @@
+package request;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class RequestBuilder {
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+    private static final List<String> allowedMethods = List.of(GET, POST);
+
+    public static Request build(BufferedInputStream in, BufferedOutputStream out) throws IOException {
+        final var limit = 4096;
+        in.mark(limit);
+        final var buffer = new byte[limit];
+        final var read = in.read(buffer);
+
+        // ищем request line
+        final var requestLineDelimiter = new byte[]{'\r', '\n'};
+        final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+        if (requestLineEnd == -1) {
+            badRequest(out);
+            return null;
+        }
+
+        // читаем request line
+        final var requestLine = new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
+        if (requestLine.length != 3) {
+            badRequest(out);
+            return null;
+        }
+
+        final var method = requestLine[0];
+        if (!allowedMethods.contains(method)) {
+            badRequest(out);
+            return null;
+        }
+        System.out.println(method);
+
+        final var path = requestLine[1];
+        if (!path.startsWith("/")) {
+            badRequest(out);
+            return null;
+        }
+        System.out.println(path);
+
+        // ищем заголовки
+        final var headersDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
+        final var headersStart = requestLineEnd + requestLineDelimiter.length;
+        final var headersEnd = indexOf(buffer, headersDelimiter, headersStart, read);
+        if (headersEnd == -1) {
+            badRequest(out);
+            return null;
+        }
+
+        // отматываем на начало буфера
+        in.reset();
+        // пропускаем requestLine
+        in.skip(headersStart);
+
+        final var headersBytes = in.readNBytes(headersEnd - headersStart);
+        final var headers = Arrays.asList(new String(headersBytes).split("\r\n"));
+        System.out.println(headers);
+
+        // для GET тела нет
+        String body = null;
+        if (!method.equals("GET")) {
+            in.skip(headersDelimiter.length);
+            // вычитываем Content-Length, чтобы прочитать body
+            final var contentLength = extractHeader(headers, "Content-Length");
+            if (contentLength.isPresent()) {
+                final var length = Integer.parseInt(contentLength.get());
+                final var bodyBytes = in.readNBytes(length);
+
+                body = new String(bodyBytes);
+                System.out.println(body);
+            }
+        }
+
+//        out.write((
+//                """
+//                        HTTP/1.1 200 OK\r
+//                        Content-Length: 0\r
+//                        Connection: close\r
+//                        \r
+//                        """
+//        ).getBytes());
+//        out.flush();
+
+        return new Request(method, path, headers, body);
+    }
+
+    private static Optional<String> extractHeader(List<String> headers, String header) {
+        return headers.stream()
+                .filter(o -> o.startsWith(header))
+                .map(o -> o.substring(o.indexOf(" ")))
+                .map(String::trim)
+                .findFirst();
+    }
+
+    private static void badRequest(BufferedOutputStream outputStream) throws IOException {
+        outputStream.write((
+                """
+                        HTTP/1.1 400 Bad Request\r
+                        Content-Length: 0\r
+                        Connection: close\r
+                        \r
+                        """
+        ).getBytes());
+        outputStream.flush();
+    }
+
+    private static int indexOf(byte[] array, byte[] target, int start, int max) {
+        outer:
+        for (int i = start; i < max - target.length + 1; i++) {
+            for (int j = 0; j < target.length; j++) {
+                if (array[i + j] != target[j]) {
+                    continue outer;
+                }
+            }
+            return i;
+        }
+        return -1;
+    }
+}
